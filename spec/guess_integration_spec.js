86d2f3ecdd05108f208b1e761fbba5c99af37187
mespresso <1249373816@qq.com>
2013-11-20 16:45:02 +0800

    change guess_game position

--- spec/guess_integration_spec.js
+++ spec/guess_integration_spec.js
@@ -11,12 +11,12 @@ describe("user can guess a number compare with the random number",function(){
 			return "0A4B";
 		}
 	}
-	guess = new Guess(random_mock_func,compare_mock_func);
 	it("1234 compare with random number(4321) will got 0A4B",function(){
+		guess = new Guess(random_mock_func,compare_mock_func);
 		expect(guess.guess(1234)).toEqual("0A4B");
 	});
-	guess = new Guess(random_mock_func,compare);
 	it("1234 compare with random number(4321) will got 0A4B",function(){
+		guess = new Guess(random_mock_func,compare);
 		expect(guess.guess(1234)).toEqual("0A4B");
 	});
 });
